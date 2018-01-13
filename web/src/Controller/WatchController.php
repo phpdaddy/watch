913f3c42cd16666762f232d5808db1f49523de11
Maksym Prysiazhnyi <m.prysiazhnyi@gmail.com>
2018-01-13 12:07:24 +0100

    Add dataProvider parameter and logic to controller

--- web/src/Controller/WatchController.php
+++ web/src/Controller/WatchController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\DataProvider\MySql\MySqlRepositoryException;
+use App\DataProvider\MySql\MySqlWatchRepository;
+use App\DataProvider\Xml\XmlLoaderException;
+use App\DataProvider\Xml\XmlWatchLoader;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -11,10 +15,38 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class WatchController extends Controller
 {
     /**
+     * @var MySqlWatchRepository
+     */
+    private $mySqlWatchRepository;
+    /**
+     * @var XmlWatchLoader
+     */
+    private $xmlWatchLoader;
+
+    public function __construct(MySqlWatchRepository $mySqlWatchRepository, XmlWatchLoader $xmlWatchLoader)
+    {
+        $this->mySqlWatchRepository = $mySqlWatchRepository;
+        $this->xmlWatchLoader = $xmlWatchLoader;
+    }
+
+    /**
      * @Route("/{id}", requirements={"id" = "\d+"}, defaults={"id" = 1})
      */
     public function getByIdAction($id)
     {
-
+        if ($this->container->getParameter('dataProvider') == 'xml') {
+            try {
+                return $this->xmlWatchLoader->loadByIdFromXml($id);
+            } catch (XmlLoaderException $e) {
+                return ['error' => $e->getMessage()];
+            }
+        } else {
+            // default - mysql
+            try {
+                return $this->mySqlWatchRepository->getWatchById($id);
+            } catch (MySqlRepositoryException $e) {
+                return ['error' => $e->getMessage()];
+            }
+        }
     }
 }
